b20bddca6c267be4e9178f12f05e815fb41ca42c
Vitória Souza <viihsouza161@gmail.com>
2022-05-26 08:36:14 -0300

    added SSG and props in subscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Head from "next/head"
 import { SubscribeButton } from "../components/SubscribeButton/SubscribeButton"
 import { stripe } from "../services/stripe"
@@ -33,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1L3VKQHpTFPD33yXHfBzzGQU", { expand: ["product"] })
   const product = {
     priceId: price.id,
@@ -43,6 +43,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24hrs
   }
 }
