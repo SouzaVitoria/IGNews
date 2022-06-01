a49bff0ad2e9fb3111a85568ad1803e128397ec2
Vitória Souza <viihsouza161@gmail.com>
2022-06-01 15:29:51 -0300

    faunadb: added login and email exists condition

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   providers: [
@@ -13,4 +15,39 @@ export default NextAuth({
       }
     })
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user.user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+
+    }
+  }
 })
